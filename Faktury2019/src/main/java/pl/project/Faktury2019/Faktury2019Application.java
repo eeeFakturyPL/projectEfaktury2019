e0fe403a07493cdeb354402bea008c033aac13db
Mariusz <eeeFakturyPL@gmail.com>
2019-11-16 11:59:01 +0100

    struktura projektu

---
+++ Faktury2019/src/main/java/pl/project/Faktury2019/Faktury2019Application.java
@@ -0,0 +1,13 @@
+package pl.project.Faktury2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Faktury2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Faktury2019Application.class, args);
+	}
+
+}
---
+++ Faktury2019/src/test/java/pl/project/Faktury2019/Faktury2019ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.project.Faktury2019;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Faktury2019ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
