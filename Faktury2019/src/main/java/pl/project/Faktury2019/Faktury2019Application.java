bdfb53608e12e9142f71fab81abcda2167f1029f
Mateusz Fiolek <eeeFakturyPL@gmail.com>
2019-11-16 13:05:03 +0100

    projekt start

--- Faktury2019/src/main/java/pl/project/Faktury2019/Faktury2019Application.java
+++ Faktury2019/src/main/java/pl/project/Faktury2019/Faktury2019Application.java
@@ -8,6 +8,7 @@ public class Faktury2019Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Faktury2019Application.class, args);
+		System.out.println("test");
 	}
 
 }
