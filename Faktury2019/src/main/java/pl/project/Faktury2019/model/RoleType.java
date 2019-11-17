f4f8a210e2d20067ca3411e5f40159fcab262ca2
Mateusz Fiolek <eeeFakturyPL@gmail.com>
2019-11-17 10:57:13 +0100

    thymeleaf

---
+++ Faktury2019/src/main/java/pl/project/Faktury2019/model/RoleType.java
@@ -0,0 +1,7 @@
+package pl.project.Faktury2019.model;
+
+public enum RoleType {
+    ROLE_READ_ONLY,
+    ROLE_USER,
+    ROLE_ADMIN;
+}
--- Faktury2019/src/main/java/pl/project/Faktury2019/repository/LoginUserRepository.java
+++ Faktury2019/src/main/java/pl/project/Faktury2019/repository/LoginUserRepository.java
@@ -2,9 +2,9 @@ package pl.project.Faktury2019.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pl.project.Faktury2019.model.entity.LoginUser;
+import pl.project.Faktury2019.model.entity.UserDetail;
 
-public interface LoginUserRepository extends JpaRepository<LoginUser,Long> {
+public interface LoginUserRepository extends JpaRepository<UserDetail,Long> {
 
 
     Long countByLogin(String login);
