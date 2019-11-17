794b0f32711c1ccdaf3644748bd071f7f1dd2e9a
Mateusz Fiolek <eeeFakturyPL@gmail.com>
2019-11-17 11:47:39 +0100

    1

--- Faktury2019/src/main/java/pl/project/Faktury2019/controller/LoginController.java
+++ Faktury2019/src/main/java/pl/project/Faktury2019/controller/LoginController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
-    @RequestMapping(value = {"index"})
-    public String index(){
-        return "login";
+public class LoginController {
+    @RequestMapping("/rejestracja")
+    public String login(){
+        return "rejestracja";
     }
 }
